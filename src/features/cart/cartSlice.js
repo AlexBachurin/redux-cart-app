b09e49d21c2ca58ae5d6cd5bd7cd643475126337
Alex <alexbachurin91@gmail.com>
2022-03-29 04:01:56 +0300

    remove item functionality

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -10,11 +10,20 @@ const cartSlice = createSlice({
     name: 'cart',
     initialState,
     reducers: {
+        //clear cart
         clearCart: (state) => {
             state.cartItems = [];
+        },
+        //remove single item, destructure payload right away
+        removeItem: (state, { payload }) => {
+            console.log(payload)
+            const filtered = state.cartItems.filter(item => item.id !== payload);
+            state.cartItems = [...filtered];
         }
+        //increase
+        //decrease
     }
 })
-console.log(cartSlice)
-export const { clearCart } = cartSlice.actions;
+
+export const { clearCart, removeItem } = cartSlice.actions;
 export default cartSlice.reducer;
